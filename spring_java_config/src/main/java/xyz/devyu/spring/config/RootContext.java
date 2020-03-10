0745ab54feb6c9fb6b57866dca2541e8348abe6a
puregyu <puregyu@naver.com>
2020-03-11 01:39:21 +0900

    spring java config

---
+++ spring_java_config/src/main/java/xyz/devyu/spring/config/RootContext.java
@@ -0,0 +1,21 @@
+package xyz.devyu.spring.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class RootContext {
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/스키마");
+		dataSource.setUsername("아이디");
+		dataSource.setPassword("비밀번호");
+		return dataSource;
+	}
+}
